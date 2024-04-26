92cd68dab4632d73937e0bd4779415e79f1cae80
Dhruhi Sheth <shethdhruhi05@gmail.com>
2024-04-26 15:56:51 -0700

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -31,8 +31,8 @@ public class FamilyTree
         
         void addChild(TreeNode childNode)
         {
-            // Add childNode to this node's children list. Also
-            // set childNode's parent to this node.
+            children.add(childNode);// Add childNode to this node's children list. Also
+            childNode.parent=this;// set childNode's parent to this node.
         }
         
         
@@ -41,14 +41,15 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
             for (TreeNode child: children)
             {
-                // If child.getNodeWithName(targetName) returns a non-null node,
-                // then that's the node we're looking for. Return it.
+                TreeNode result= child.getNodeWithName(targetName);// If child.getNodeWithName(targetName) returns a non-null node,
+                if (result!=null)
+                	return result;// then that's the node we're looking for. Return it.
             }
             
             // Not found anywhere.
@@ -61,15 +62,25 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
+            TreeNode currentNode = this;
+            while (currentNode.parent != null) {
+                	ancestors.add(currentNode.parent);
+                	currentNode = currentNode.parent;
+            	}
+            
+            	return ancestors;
+            	
+    }
+    
+
+            
 
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
 
-            return ancestors;
-        }
-        
+             
         
         public String toString()
         {
@@ -109,8 +120,8 @@ public class FamilyTree
 
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
+		FileReader fr = new FileReader(treeFile);
+		BufferedReader br = new BufferedReader (fr);
 		String line;
 		while ((line = br.readLine()) != null)
 			addLine(line);
